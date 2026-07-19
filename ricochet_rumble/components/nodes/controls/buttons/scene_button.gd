3cf775db348006ac8de390e9534b45496be8ecf7
DevGreem <ian.pichardo@hotmail.com>
2026-07-19 15:35:15 -0400

    feat: Added icon and updated README.md

--- ricochet_rumble/components/nodes/controls/buttons/scene_button.gd
+++ ricochet_rumble/components/nodes/controls/buttons/scene_button.gd
@@ -12,7 +12,9 @@ class_name SceneButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.pressed.connect(_on_pressed)
+	
+	if not pressed.is_connected(_on_pressed):
+		self.pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
 	

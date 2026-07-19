8961f234638f495e71e54c7a879008c580351b0a
Miguel Franjul <144841890+LeviathanGamez@users.noreply.github.com>
2026-07-18 23:44:49 -0400

    Update upgrade_item.gd

--- ricochet_rumble/content/items/upgrades/item/upgrade_item.gd
+++ ricochet_rumble/content/items/upgrades/item/upgrade_item.gd
@@ -1,12 +1,15 @@
 extends Sprite2D
 
 @export var upgrade: PlayerUpgrades2
+
 var bullet_count = preload("res://content/items/upgrades/resources/number_of_bullets.tres")
 var fire_rate = preload("res://content/items/upgrades/resources/fire_rate.tres")
 var damage = preload("res://content/items/upgrades/resources/damage.tres")
 var bullet_speed = preload("res://content/items/upgrades/resources/bullet_speed.tres")
 var more_bounces = preload("res://content/items/upgrades/resources/more_bounces.tres")
-var random_upgrade = [fire_rate,bullet_count,damage,bullet_speed,more_bounces].pick_random()
+var speed = preload("res://content/items/upgrades/resources/speed.tres")
+
+var random_upgrade = [fire_rate,bullet_count,damage,bullet_speed,more_bounces,speed].pick_random()
 #var random_upgrade = [bullet_speed,more_bounces].pick_random()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
